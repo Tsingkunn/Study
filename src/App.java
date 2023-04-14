d12217cb569733d2afcb73aee4084e1fb7911241
lucia <15928834149@139.com>
2023-04-14 16:22:26 +0800

    JavaStudy

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/**
+ * 计算两个整数之和
+ */
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(1.0 / 3);
+        System.out.println(1 % 3);
+        System.out.println(1 + 2);
+        int num = 1;
+        System.out.println(num);
+        System.out.println("../bin/App.class");
+    }
+}
